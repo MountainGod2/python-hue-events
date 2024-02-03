be2519f6816a3980ba78e1cceaf83d12835e1768
MountainGod2 <admin@reid.ca>
2024-02-03 10:10:45 -0700

    Update requirements

--- src/main.py
+++ src/main.py
@@ -8,22 +8,12 @@ import dotenv
 import logging
 
 
-dotenv.load_dotenv(dotenv_path="../.env")
-
 logging.basicConfig(level=logging.DEBUG)
 
 
-class APIConfig:
-    username: str = os.getenv("USERNAME", "")
-    token: str = os.getenv("TOKEN", "")
-    base_url: str = os.getenv("BASE_URL", "https://eventsapi.chaturbate.com/events")
-    request_timeout: int = int(os.getenv("TIMEOUT", "30"))
-    url = str(f"{base_url}{username}/{token}?timeout={request_timeout}")
-
-
 async def main():
-    # Load API configuration
-    api_config = APIConfig()
+    dotenv.load_dotenv()
+    url = os.getenv("API_URL")
 
     # Initialize the Hue Bridge and Light Controller
     hue_bridge = HueBridge()
@@ -33,7 +23,7 @@ async def main():
     event_handler = EventHandler()
 
     # Initialize the Event Poller with the required arguments
-    event_poller = EventPoller(api_config.url, api_config.request_timeout)
+    event_poller = EventPoller(url, 20)
 
     # Start polling events and processing them
     await event_handler.process_events(event_poller.poll_events(), light_controller)
