beaf9e1887d1ee214eee397d179e1440f5b0637a
MountainGod2 <admin@reid.ca>
2024-02-04 12:40:14 -0700

    Add LICENSE and README.md

---
+++ README.md
@@ -0,0 +1,32 @@
+# Python Hue Events
+
+## Description
+Python Hue Events is a project that allows users to control their Philips Hue lights in response to updates from the Chaturbate Events API. The application polls events, processes them, and then commands the lights based on these events. (By default, it flashes the lights green when a fan club member joins the room.)
+
+## Installation
+To install Python Hue Events, follow these steps:
+1. Clone the repository to your local machine.
+2. Install the required dependencies by running `pip install -r requirements.txt` in your terminal.
+3. Create a API token here: `https://chaturbate.com/statsapi/authtoken/` (Ensure you select the "Events API" checkbox).
+4. Follow the program instructions to setup your .env file and Hue credentials.
+
+## Usage
+After installation, you can run the project by executing the `main.py` script in the `src` directory. The application performs the following tasks:
+- Initializes the connection with the Philips Hue Bridge.
+- Sets up light control mechanisms.
+- Polls the Chaturbate Events API for updates.
+- Handles the received events to control the lighting based on predefined logic.
+
+## Files and Functionality
+- `main.py`: Initializes the application, sets up logging, loads environment variables, and runs the main event loop.
+- `hue_bridge.py`: Manages communication with the Philips Hue Bridge, including light control commands.
+- `light_controller.py`: Integrates with `hue_bridge.py` to control the behavior of the lights.
+- `event_handler.py`: Processes the events received from the polling mechanism and decides the light behavior.
+- `event_poller.py`: Continuously polls the Chaturbate Events API and manages error handling with retry mechanisms.
+
+## Contributing
+Contributions are welcome. Feel free to fork the repository, make changes, and submit a pull request.
+
+## License
+This project is licensed under the MIT License.
+
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 import os
-
+import re
 import dotenv
 
 from constants import API_TIMEOUT
@@ -27,15 +27,41 @@ logging.getLogger("zeroconf").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
+def extract_user_and_token(url):
+    # Extract username and token from the URL using regular expressions
+    match = re.match(r"https://eventsapi.chaturbate.com/events/([^/]+)/([^/]+)/", url)
+    if match:
+        return match.group(1), match.group(2)
+    return None, None
+
+
+def prompt_for_api_url_and_save():
+    while True:
+        api_url = input("Enter your Chaturbate API token URL: ")
+        username, token = extract_user_and_token(api_url)
+        if username and token:
+            # Save the username and token to the .env file
+            with open(".env", "a") as env_file:
+                env_file.write(f"USERNAME={username}\n")
+                env_file.write(f"TOKEN={token}\n")
+            return username, token
+        else:
+            print("Invalid URL format. Please enter a valid Chaturbate API token URL.")
+
+
 async def main():
     try:
         # Load the environment variables for the username and token
         dotenv.load_dotenv()
-        username = os.getenv("USERNAME")
+        user = os.getenv("USERNAME")
         token = os.getenv("TOKEN")
 
+        # Check if username and token are set, and if not, prompt the user for the API token URL
+        if not user or not token:
+            user, token = prompt_for_api_url_and_save()
+
         # Construct the URL for the Chaturbate Events API
-        url = f"https://eventsapi.chaturbate.com/events/{username}/{token}"
+        url = f"https://eventsapi.chaturbate.com/events/{user}/{token}"
 
     except Exception as e:
         logging.getLogger("Main").error(f"Error loading environment variables: {e}")
@@ -45,8 +71,8 @@ async def main():
     try:
         # Initialize the Hue Bridge and Light Controller
         logging.getLogger("Main").debug("Initializing Hue Bridge and Light Controller.")
-        hue_bridge = HueBridge()
-        light_controller = LightController(hue_bridge)
+        hue = HueBridge()
+        light_ctrl = LightController(hue)
 
         logging.getLogger("Main").debug("Initializing Event Handler and Poller.")
         # Initialize the Event Handler
@@ -57,7 +83,7 @@ async def main():
 
         logging.getLogger("Main").info("Starting program.")
         # Start polling events and processing them
-        await event_handler.process_events(event_poller.poll_events(), light_controller)
+        await event_handler.process_events(event_poller.poll_events(), light_ctrl)
 
     except Exception as e:
         logging.getLogger(__name__).exception(e)
@@ -66,6 +92,7 @@ async def main():
         logging.getLogger(__name__).info("Exiting due to keyboard interrupt.")
 
     finally:
+        logging.getLogger("Main").info("Shutting down.")
         # Align the log entries
         log_aligner = LogAligner(file_path="app.log", delete_original=True)
         await log_aligner.align_log_entries()
